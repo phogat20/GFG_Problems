c32c6b25a77aaf0d8234386e36c42cec41ca8908
CHETAN SINGH PHOGAT <84695818+phogat20@users.noreply.github.com>
2024-08-24 07:36:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.java
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.java
@@ -37,29 +37,26 @@ class Solution {
     // Function to return Breadth First Traversal of given graph.
     public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
         // Code here
+    ArrayList<Integer> bfs = new ArrayList<>();
+    boolean[] vis = new boolean[V];
+    Queue<Integer> q = new LinkedList<>();
+    
+    vis[0] =true;  //true
+    q.add(0); // 0
         
-     ArrayList<Integer> bfs = new ArrayList<>();
-     boolean vis[] = new boolean[V];
-     Queue <Integer> q = new LinkedList<>();
-     
-     // Start BFS from the first node (0)
-     q.add(0);
-     vis[0] = true;
-     
-     while(!q.isEmpty()){                                          
-        Integer node = q.poll(); // remove a node from the queue
-        bfs.add(node); // Add it to the BFS result list
-         
-        // Traverse all adjacent nodes of the node removed
-            for (Integer it : adj.get(node)) {
-        // If the adjacent node is not visited, mark it visited and enqueue it
-             if(vis[it] == false){
-                 vis[it] = true;
-                 q.add(it);
-             }
-         }
-     }
-     return bfs;
+    while(!q.isEmpty()){
+        Integer node = q.poll();  // 0
+        bfs.add(node);
+        
+        for(Integer it: adj.get(node)){ // 0
+            if(vis[it] == false){
+                vis[it] = true;
+                q.add(it);
+            }
+        }
+    }    
+        
+       return bfs; 
      // TC - O(n) + O(2E)
      // SC - O(3N) = O(N) (bfs, q, vis)
     }
