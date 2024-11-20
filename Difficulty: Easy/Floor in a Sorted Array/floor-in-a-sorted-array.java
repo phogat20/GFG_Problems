ff9659e6bf0563b37417c8ca00bd5701ecad344b
CHETAN SINGH PHOGAT <84695818+phogat20@users.noreply.github.com>
2024-11-20 23:00:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Floor in a Sorted Array/floor-in-a-sorted-array.java
+++ Difficulty: Easy/Floor in a Sorted Array/floor-in-a-sorted-array.java
@@ -1,78 +1,71 @@
 //{ Driver Code Starts
-import java.util.*;
-import java.lang.*;
+// Initial Template for Java
 import java.io.*;
+import java.util.*;
+import java.util.HashMap;
 
-class Searching
-{
-    public static void main (String[] args)throws IOException {
 
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter out=new PrintWriter(System.out);
-        int t = Integer.parseInt(read.readLine());
-        
-        while(t-- > 0)
-        {
-           String a[] = read.readLine().trim().split("\\s+");
-           int n = Integer.parseInt(a[0]);
-           long x = Long.parseLong(a[(int)1]);
-           String st[] = read.readLine().trim().split("\\s+");
-        
-           long arr[] = new long[n];
-           for(int i = 0; i < n; i++)
-           {
-               arr[i] = Long.parseLong(st[i]);
-           }
-           
-           out.println(new Solution().findFloor(arr, n, x));
-          
-        }
-        out.close();
-    }
-    
-}
 // } Driver Code Ends
+class Solution {
 
-
-class Solution{
-    
-    // Function to find floor of x
-    // arr: input array
-    // n is the size of array
-    static int findFloor(long arr[], int n, long x)
-    {
-        int l = 0;
-        int h = n-1;
-//   while(start <= end){
-//             int mid = start + (end - start)/2;
-//             if(x > arr[mid]){
-//                 start = mid + 1;
-//             } else if (x < arr[mid]){
-//                 end = mid - 1;
-//             } else{
-//                 return mid;
-//             }
-//         }
+    static int findFloor(int[] arr, int k) {
+        // write code here
+        if(k<arr[0]){
+            return -1;
+        }
         
-//         return end;
-        
-        // if(arr[0]>x) return -1;
-        // if(arr[n-1]<x) return n;
+        int l =0;
+        int h = arr.length-1;
         
         while(l<=h){
-            int mid = l + (h-l)/2;
-
-            if(x<arr[mid]){
-                h = mid-1;
-    
-            }else if(x>arr[mid]){
-                l = mid+1;
-        }else{
-            return mid;
-        }
+            int mid = (l+h)/2;
             
+            if(k<arr[mid]){
+                h = mid-1;
+            }else if(k>arr[mid]){
+                l = mid+1;
+            }else{
+                return mid;
+            }
         }
         return h;
     }
-    
 }
+
+
+//{ Driver Code Starts.
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            int k = Integer.parseInt(br.readLine());
+            // Create Solution object and find closest sum
+            Solution ob = new Solution();
+            int ans = ob.findFloor(arr, k);
+
+            System.out.print(ans);
+
+            System.out.println(); // New line after printing the results
+        }
+    }
+}
+
+// } Driver Code Ends
